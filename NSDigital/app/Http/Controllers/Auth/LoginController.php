bda96ba71ea3718623c8bdda9c04545dcd76e5f5
MrWilllian <willian.dev10@gmail.com>
2020-02-18 09:08:58 -0300

    refactor: change method view name

--- NSDigital/app/Http/Controllers/Auth/LoginController.php
+++ NSDigital/app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function loginView() {
+    public function index() {
         return view('login.index');
     }
 }
