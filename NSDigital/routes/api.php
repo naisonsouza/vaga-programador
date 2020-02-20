604b5411f5c6e0acff4c45770273bcc3a9f0652b
MrWilllian <willian.dev10@gmail.com>
2020-02-20 08:49:20 -0300

    refactor: add api routes for Models (Artist, Album and Music)

--- NSDigital/routes/api.php
+++ NSDigital/routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('artist', 'Dashboard\ArtistController');
+Route::resource('album', 'Dashboard\AlbumController');
+Route::resource('music', 'Dashboard\MusicController');
