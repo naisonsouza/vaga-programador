58fd39b5878a48af4cbf88ef01ccfc4fcb2a7b07
MrWilllian <willian.dev10@gmail.com>
2020-02-23 18:21:57 -0300

    chore: final changes in buttons of Edit, View and Remove

--- NSDigital/resources/views/dashboard/artists/index.blade.php
+++ NSDigital/resources/views/dashboard/artists/index.blade.php
@@ -12,8 +12,8 @@
     </div>
 
     <div class="row container-table-artists">
-      <div class="col-md-12 table-responsive">
-        <table class="table table-dark table-bordered table-hover">
+      <div class="col-md-12 table-responsive table-wrapper-scroll-y my-custom-scrollbar">
+        <table class="table table-dark table-bordered table-hover mb-0">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -23,18 +23,27 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>
-                <img src="{{url('uploads/ns_logo.png')}}" />
-              </td>
-              <td>Mark</td>
-              <td>
-                <a href="{{ route('new-artist') }}" class="button-action">
-                  <i class="fas fa-trash"></i>
-                </a>
-              </td>
-            </tr>
+            @foreach ($artists as $artist)
+              <tr id="{{$artist->id}}">
+                <th scope="row">{{$artist->id}}</th>
+                <td>
+                  <img class="img_artist"
+                    src="{{url('storage/uploads/artist/'.$artist->filename)}}" alt="{{$artist->filename}}" />
+                </td>
+                <td>{{$artist->name}}</td>
+                <td class="actions">
+                  <a href="#" id="{{$artist->id}}" class="button-action edit_button">
+                    <i class="fas fa-edit"></i>
+                  </a>
+                  <a href="#" id="{{$artist->id}}" class="button-action view_button">
+                    <i class="fas fa-search"></i>
+                  </a>
+                  <a id="{{$artist->id}}" href="#" class="button-action remove_button">
+                    <i class="fas fa-trash"></i>
+                  </a>
+                </td>              
+              </tr>
+            @endforeach
           </tbody>
         </table>
         
@@ -44,6 +53,45 @@
         <i class="fas fa-plus-circle"></i>
         Criar Novo Artista
       </a>
+    </div>
+
+    <input type="hidden" id="page" value="artist" />
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+      <!-- Modal content -->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2>Editar</h2>
+          <span class="close">&times;</span>
+        </div>
+        <div class="modal-body">
+          @include('dashboard.artists.artist-form', ['page' => 'edit'])
+        </div>
+        <div class="modal-footer">
+          <br /><br />
+        </div>
+      </div>
+
+    </div>
+
+    <div id="viewModal" class="modal">
+
+      <!-- Modal content -->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2>Visualizar</h2>
+          <span class="close">&times;</span>
+        </div>
+        <div class="modal-body">
+          @include('dashboard.artists.artist-form', ['page' => 'view'])
+        </div>
+        <div class="modal-footer">
+          <br /><br />
+        </div>
+      </div>
+
     </div>
 
   </section>
