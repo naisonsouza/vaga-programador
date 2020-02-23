2ce4735e821640635180b0158230f9c834ffd92a
MrWilllian <willian.dev10@gmail.com>
2020-02-23 18:26:33 -0300

    chore: add scripts in global footer-scripts with the ajax methods to remove, edit and view artists/albuns

--- NSDigital/resources/views/layouts/dashboard/partials/footer-scripts.blade.php
+++ NSDigital/resources/views/layouts/dashboard/partials/footer-scripts.blade.php
@@ -3,31 +3,70 @@
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.4.0/bootbox.min.js"></script> --}}
 
 <script>
-  // $(document).on("click", document.getElementById('btn-add-music'), function(e) {
-  // $(document).ready(function(){ 
-  //   $('#btn-add-music').click(function(e) {
-  //     console.log('bootbox', bootbox);
+  var modal = document.getElementById("myModal");
+  var viewModal = document.getElementById("viewModal");
+  var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("close")[1];
 
-  //     bootbox.confirm("Are you sure?", function(confirmed) {
-  //       console.log('entrou');
-  //     });
-  //   });
-    
-  // });
-  
-  var $input = document.getElementById('image-album'), 
-      $fileName = document.getElementById('file-name');
+  span.onclick = function() { modal.style.display = "none"; }
+  span2.onclick = function() { viewModal.style.display = "none"; }
+  window.onclick = function(event) {
+    if (event.target == modal || event.target == viewModal) { 
+      modal.style.display = "none"; 
+      viewModal.style.display = "none"; 
+    }
+  }
+  var page = document.getElementById('page').value;
 
-  var $inputMusic = document.getElementById('music-file'), 
-    $musicName = document.getElementById('music-name');
-  
-  $(document).on('change', $input, function() {
-    $fileName.textContent = $input.value;
+  $('.edit_button').click(function() {
+    var id = $(this).attr('id');
+    $.ajax({
+      type: 'get', url: 'http://127.0.0.1:8000/api/'+page+'/'+id+'/edit',
+      success: function (data) {
+        $('.artist_form').attr('id', 'artist_form_edit');
+        $('#nameHelp').val(data.name);
+        $('#artist_form_edit').attr('id', 'artist_form_edit');
+        $('#artist_form_edit').attr('action', 'http://127.0.0.1:8000/api/'+page+'/'+data.id);
+        modal.style.display = "block";
+      },
+      error: function (data) { console.log(data); }
+    });
   });
 
-  $(document).on('change', $inputMusic, function() {
-    $musicName.textContent = $inputMusic.value;
+  $('.remove_button').click(function() {
+    var tr = $(this).closest('tr'), del_id = $(this).attr('id');
+
+    if(confirm("Tem certeza que deseja deletar esse artista? "))
+    { 
+      $.ajax({
+        type: 'delete', url: 'http://127.0.0.1:8000/api/' + page + '/' + del_id,
+        success: function (data) { tr.fadeOut(1000, function(){ $(this).remove(); }); },
+        error: function (data) { console.log(data); }
+      });
+    }
   });
 
+  $('.view_button').click(function() { 
+    var id = $(this).attr('id');
+    $.ajax({
+      type: 'get', url: 'http://127.0.0.1:8000/api/'+page+'/'+id,
+      success: function (data) {
+        $('.artist_form').attr('id', 'artist_form_view');
+        $('#name_view').val(data.name);
+        $('#name_view').prop('disabled', true);
+        $('#img_artist_view').attr('src', 'storage/uploads/artist/'+data.filename);
+        $('#img_artist_view').attr('alt', data.original_filename);
+        
+        viewModal.style.display = "block";
+      },
+      error: function (data) { console.log(data); }
+    });
+  });
+  
+  var $input = document.getElementById('image-album'), $fileName = document.getElementById('file-name');
+  var $inputMusic = document.getElementById('music_file'), $musicName = document.getElementById('music-name');
+  
+  $(document).on('change', $input, function() { $fileName.textContent = $input.value; });
+  $(document).on('change', $inputMusic, function() { $musicName.textContent = $inputMusic.value; });
 
 </script>
