49470989239b9b29ecb6aa887e933ae65d657c45
MrWilllian <willian.dev10@gmail.com>
2020-02-23 19:26:20 -0300

    chore: inserction AlbumRepository in AlbumController

--- NSDigital/app/Http/Controllers/Dashboard/AlbumController.php
+++ NSDigital/app/Http/Controllers/Dashboard/AlbumController.php
@@ -4,9 +4,23 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Repositories\AlbumRepository;
 
 class AlbumController extends Controller
 {
+    protected $albuns;
+
+    /**
+     * Create a new repository instance.
+     *
+     * @param  AlbumRepository  $albuns
+     * @return void
+     */
+    public function __construct(AlbumRepository $albuns)
+    {
+        $this->albuns = $albuns;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -45,7 +59,32 @@ class AlbumController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            dd($request);
+
+            $validatedData = $request->validate([
+                'title_album'=>'required',
+            ]);
+            
+            $image = $request->file('image-album');
+            $extension = $image->getClientOriginalExtension();
+            $validatedData->original_filename = $image->getClientOriginalName();
+            $validatedData->filename = $image->getFilename().'.'.$extension;
+        
+            Storage::disk('storage')->put(
+                $image->getFilename().'.'.$extension, File::get($image)
+            );
+
+            $album_id = $this->albuns->newAlbum($validatedData)->id;
+            
+            $music_archive = $request->file('music_file');
+            $music_name = $request->title_music;            
+
+            return response()->json(['success', 200]);
+            
+        } catch(Exception $e) {
+            return response()->json(['error' => $e->getMessage(), 401]);
+        }
     }
 
     /**
@@ -74,22 +113,29 @@ class AlbumController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Album  $album
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Album $album)
+    public function update(Request $request)
     {
-        //
+        if ($this->albuns->updateAlbum($request) == 1) {
+            return response()->json(['success', 200]);
+        } else {
+            return response()->json(['error' => $e->getMessage(), 401]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Album  $album
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Album $album)
+    public function destroy($id)
     {
-        //
+        if ($this->albuns->destroyAlbum($id) == 1) {
+            return response()->json([ 'message' => 'Successful' ], 200);
+        } else {
+            return response()->json([ 'message' => 'Error' ], 404);
+        }
     }
 }
