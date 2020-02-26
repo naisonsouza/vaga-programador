37189aff88dfc82a75208df0844c5cf15a25a7a4
MrWilllian <willian.dev10@gmail.com>
2020-02-26 10:18:45 -0300

    chore: add modal actions and replace to new album page with artist id

--- NSDigital/resources/views/dashboard/albuns/album-scripts.blade.php
+++ NSDigital/resources/views/dashboard/albuns/album-scripts.blade.php
@@ -1,4 +1,31 @@
 <script>
+  var modal = document.getElementById("modalArtists");
+  var span = document.getElementsByClassName("close")[0];
+
+  if (span) {
+    span.onclick = function() { modal.style.display = "none"; }
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) { modal.style.display = "none"; }
+  }
+
+  $('#btn-create-album').click(function() {
+    $.ajax({
+      type: 'get', url: 'http://127.0.0.1:8000/api/artists/list',
+      success: function (data) {
+        data.forEach(function(artist) {
+          $('#list_artits').append('<option value="'+artist.id+'">'+artist.name+'</option>');
+        });
+        modal.style.display = "block";
+      },
+      error: function (data) { console.log(data); }
+    });
+  });
+
+  $('#btn-select-artist').click(function() {
+    window.location.replace("http://127.0.0.1:8000/new-album?id="+$("#list_artits option:selected").val());
+  });
+
   $('#btn-add-music').click(function() {
     $('#musics_table > tbody:last-child').append(
       '<tr><td scope="row"><i class="fa fa-music"></i></td>'
