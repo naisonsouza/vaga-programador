dc7ea37e00dc38fe2d9193c7182984b7cfdc54c0
MrWilllian <willian.dev10@gmail.com>
2020-02-26 20:40:48 -0300

    chore: add id @param in update method

--- NSDigital/app/Http/Controllers/Dashboard/ArtistController.php
+++ NSDigital/app/Http/Controllers/Dashboard/ArtistController.php
@@ -107,6 +107,7 @@ class ArtistController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
