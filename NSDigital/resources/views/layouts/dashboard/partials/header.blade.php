b7a5f1367752b320fd2f6810845721db54f88eab
MrWilllian <willian.dev10@gmail.com>
2020-02-28 10:23:21 -0300

    chore: change header to responsive bootstrap classes

--- NSDigital/resources/views/layouts/dashboard/partials/header.blade.php
+++ NSDigital/resources/views/layouts/dashboard/partials/header.blade.php
@@ -1,11 +1,23 @@
-<header>
-  <h1 class="navbar-brand">NSDigital Dashboard</h1>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top">
 
-  <div class="menu">
-    <ul>
-      <li><a href="#" class="active">Início</a></li>
-      <li><a href="#">Configurações</a></li>
-      <li><a href="#">Ajuda</a></li>
+  <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapsse navbar-collapse" id="collapse_target">
+    <a class="navbar-brand"><h1 class="navbar-brand">NSDigital Dashboard</h1></a>
+    <span class="navbar-text"></span>
+
+    <ul class="navbar-nav menu">
+      <li class="nav-item">
+        <a href="#" class="nav-link">Início</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Configurações</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">Ajuda</a>
+      </li>
     </ul>
     <div class="search-bar-container">
       <input class="search-bar" type="text" placeholder="Pesquisar..." />
@@ -14,4 +26,5 @@
       </button>
     </div>
   </div>
-</header>
+
+</nav>
