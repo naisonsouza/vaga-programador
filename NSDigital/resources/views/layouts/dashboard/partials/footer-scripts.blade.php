3e56baa0109bb41a3190a5037ce0369f8cd4890b
MrWilllian <willian.dev10@gmail.com>
2020-02-28 10:24:28 -0300

    chore: change bootstrap js version

--- NSDigital/resources/views/layouts/dashboard/partials/footer-scripts.blade.php
+++ NSDigital/resources/views/layouts/dashboard/partials/footer-scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.4.0/bootbox.min.js"></script> --}}
 
 <script> 
