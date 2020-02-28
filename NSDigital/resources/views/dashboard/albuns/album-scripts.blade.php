db6dc90ab8b39c48f74d4ab0c32a39c4aa0b0024
MrWilllian <willian.dev10@gmail.com>
2020-02-28 11:42:52 -0300

    chore: add method to remove music clicking on tr

--- NSDigital/resources/views/dashboard/albuns/album-scripts.blade.php
+++ NSDigital/resources/views/dashboard/albuns/album-scripts.blade.php
@@ -1,10 +1,16 @@
 <script>
   var modal = document.getElementById("modalArtists");
   var span = document.getElementsByClassName("close")[0];
+  var modalEdit = document.getElementById("modalEditAlbum");
+  var span2 = document.getElementsByClassName("close")[1];
 
   if (span) { span.onclick = function() { modal.style.display = "none"; } }
+  if (span2) { span2.onclick = function() { modalEdit.style.display = "none"; } }
   window.onclick = function(event) {
-    if (event.target == modal) { modal.style.display = "none"; }
+    if (event.target == modal || event.target == modalEdit) {
+      modal.style.display = "none"; 
+      modalEdit.style.display = "none"; 
+    }
   }
 
   $('#btn-create-album').click(function() {
@@ -26,14 +32,13 @@
 
   $('#btn-add-music').click(function() {
     $('#musics_table > tbody:last-child').append(
-      '<tr><td scope="row"><i class="fa fa-music"></i></td>'
+      '<tr class="row_table"><td scope="row"><i class="fa fa-music"></i></td>'
       +'<td class="music_title_td">' +$('#title_music').val() +'</td>'
       +'<td class="form-input-file"><input type="file" name="music_archive_'
       +($('table > tbody > tr').length+1) +'" id="music_archive_'
       +($('table > tbody > tr').length+1) +'" class="form-control-file" />'
       +'</td></tr>'
     );
-
     $('#title_music').val('');
     $('#title_music').focus();
 
@@ -44,8 +49,60 @@
     $('#musics_title_array').val(musicsTitleArray);
   });
 
+  $('.edit_button').click(function() {
+    var id = $(this).attr('id');
+    $.ajax({
+      type: 'get', url: 'http://127.0.0.1:8000/api/album/'+id+'/edit',
+      success: function (data) {
+        $('.album_form').attr('id', 'album_form_edit');
+        $('#album_form_edit').attr('action', 'http://127.0.0.1:8000/api/album/'+id);
+
+        $('#artist_id').val(data[0].artist_id);
+        $('#title-album').val(data[0].title);
+        $('#filename').text(data[0].original_filename);
+        
+        data[0].musics.forEach(function(music) {
+          $('#musics_table tbody').append(
+            '<tr class="row_table" id="'+music.id+'"><td><i class="fa fa-music"></i></td><td>'+ music.title +'</td>'
+             +'<td><input type="text" disabled value="'+ music.title +'.mp3" /></td></tr>'
+            //  +'<td><button type="button" class="btn_remove_music">Excluir Música</button></td></tr>'
+          )
+        });
+        modalEdit.style.display = "block";
+      },
+      error: function (data) { console.log(data); }
+    });
+
+    $(document).on('change','.form-control-file', function(e){ 
+      var tr = $(this).closest('tr');
+      title = tr.find("td:eq(1)").text();
+      file = e.target.files[0];
+      id = id;
+      $.ajax({
+        type: 'post', 
+        url: 'http://127.0.0.1:8000/api/music/saveMusic',
+        data: { title: "title", file: "file", id: "id" },
+        success: function (data) {
+          console.log(data);
+        },
+        error: function (data) { console.log(data); }
+      });
+    });
+    
+    $(document).on('click','.row_table', function(e){ 
+      var tr = $(this).closest('tr'), del_id = $(this).attr('id');
+      if(confirm("Tem certeza que deseja remover essa música? "))
+      { 
+        $.ajax({
+          type: 'delete', url: 'http://127.0.0.1:8000/api/music/'+ del_id,
+          success: function (data) { tr.fadeOut(1000, function(){ $(this).remove(); }); },
+          error: function (data) { console.log(data); }
+        });
+      }
+    });
+  });  
+
   $('.remove_button').click(function() {
-    var tr = $(this).closest('tr'), del_id = $(this).attr('id');
     if(confirm("Tem certeza que deseja deletar esse álbum? "))
     { 
       $.ajax({
