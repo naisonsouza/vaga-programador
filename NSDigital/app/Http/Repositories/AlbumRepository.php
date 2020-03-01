219880053e81c55e8b89d88401371945f7c077b7
MrWilllian <willian.dev10@gmail.com>
2020-03-01 19:02:18 -0300

    chore: add order by in listAlbuns method

--- NSDigital/app/Http/Repositories/AlbumRepository.php
+++ NSDigital/app/Http/Repositories/AlbumRepository.php
@@ -11,20 +11,23 @@ class AlbumRepository extends BaseRepository
   protected $modelClass = Album::class;
 
   public function listAlbuns() {
-    return Album::with('artist')->get();
+    return Album::with('artist')->orderBy('id', 'DESC')->get();
   }
 
   public function newAlbum($data) {
     return Album::create($data);
   }
 
-  public function updateAlbum(Request $request) {
-    $album = Album::find($request->id);
-
+  public function updateAlbum(Request $request, $id) {
+    $album = Album::find($id);
+    
     if(!$album) 
       return 0;
 
-    $album->fill($request->all());
+    $album->title = $request->title;
+    $album->filename = $request->filename;
+    $album->original_filename = $request->original_filename;
+
     $album->save();
 
     return 1;
@@ -38,5 +41,9 @@ class AlbumRepository extends BaseRepository
 
     $album->delete();
     return 1;
+  }
+
+  public function findAlbum($id) {
+    return Album::with('musics')->find($id);
   }
 }
