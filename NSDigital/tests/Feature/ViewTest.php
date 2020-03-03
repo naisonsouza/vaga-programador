64d12298df223e3487e243ed992320c0ea997e97
MrWilllian <willian.dev10@gmail.com>
2020-03-02 22:01:53 -0300

    test: add ViewTest class to views system testing

---
+++ NSDigital/tests/Feature/ViewTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class ViewTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        // $response = $this->get('/');
+        $response = $this->visit('/');
+
+        $response->assertStatus(200);
+    }
+}
