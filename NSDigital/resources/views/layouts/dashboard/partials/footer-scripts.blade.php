e8f55d35e5ee0ae876cebd4e1521a4f9b217cda4
MrWilllian <willian.dev10@gmail.com>
2020-03-02 21:48:51 -0300

    chore: add toastr minified javascript

--- NSDigital/resources/views/layouts/dashboard/partials/footer-scripts.blade.php
+++ NSDigital/resources/views/layouts/dashboard/partials/footer-scripts.blade.php
@@ -1,6 +1,7 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.4.0/bootbox.min.js"></script> --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 <script> 
   var $input = document.getElementById('album_image'), 
