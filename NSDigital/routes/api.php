b974eede2e2ed9cc56734d22ed6902c786526853
MrWilllian <willian.dev10@gmail.com>
2020-03-04 18:24:46 -0300

    docs: comment api middleware

--- NSDigital/routes/api.php
+++ NSDigital/routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::resource('artist', 'Dashboard\ArtistController');
 Route::resource('album', 'Dashboard\AlbumController');
